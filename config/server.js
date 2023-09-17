de8f76949dc95ddd90469731d90c8404b8bad6f0
sheikahamed123 <sheikahamed134@gmail.com>
2023-09-17 06:37:10 +0530

    changes

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: "L87rfv2zbfeD/VHH8HlFLw==,+qPNuGq8JCLfMpcXmfpMqA==,lkryBWnCBmyRLPCKIkM2dw==,HBwbvcCxGKpjL0Tihn4aMA==",
+    keys: env.array("L87rfv2zbfeD/VHH8HlFLw==,+qPNuGq8JCLfMpcXmfpMqA==,lkryBWnCBmyRLPCKIkM2dw==,HBwbvcCxGKpjL0Tihn4aMA=="),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
